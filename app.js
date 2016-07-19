591ea8504e482f270f193f17159651adae0695d5
Kanyango <kariukikanyango@gmail.com>
2016-07-19 19:18:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ require('./passport')(app , passport);
 
 require('./routes')(app , passport);
 
-app.listen(process.env.PORT || 8080 , function(){
+app.listen(process.env.PORT, function(){
 
 });
 
